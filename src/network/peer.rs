829cea07f982e01bc50fc31095df5d9b49b28634
Kohei Taniguchi <kohei@chaintope.com>
2019-09-30 16:38:57 +0900

    Add copyright and license description
    Signed-off-by: Kohei Taniguchi <kohei@chaintope.com>

--- src/network/peer.rs
+++ src/network/peer.rs
@@ -1,3 +1,7 @@
+// Copyright (c) 2019 Chaintope Inc.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 use crate::chain::{Chain, ChainStore};
 use crate::network::{utils::codec::NetworkMessagesCodec, Error};
 use bitcoin::network::message_blockdata::GetHeadersMessage;
--- src/network/utils/codec.rs
+++ src/network/utils/codec.rs
@@ -1,3 +1,7 @@
+// Copyright (c) 2019 Chaintope Inc.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 use super::bytes::BytesMut;
 use bitcoin::{
     consensus::{deserialize_partial, encode, Encodable},
