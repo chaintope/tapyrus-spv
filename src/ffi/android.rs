cb13b3aea2816f20587a05165b57cd1e14e631ed
Kohei Taniguchi <kohei@chaintope.com>
2019-10-24 10:33:18 +0900

    Add some codes which is for running spv on android for trying.
    Signed-off-by: Kohei Taniguchi <kohei@chaintope.com>

--- src/ffi/android.rs
+++ src/ffi/android.rs
@@ -10,12 +10,17 @@ use self::jni::JNIEnv;
 use self::jni::objects::{JClass, JString};
 use self::jni::sys::{jstring};
 use crate::ffi::c::rust_greeting;
+use bitcoin::Network;
 use std::ffi::CString;
 use log::Level;
 use crate::{Options, ChainParams, SPV};
 use android_logger::{Config, FilterBuilder};
 
-fn native_activity_create() {
+#[no_mangle]
+pub extern "system" fn Java_com_chaintope_tapyrus_spv_FFI_enableLog(
+    env: JNIEnv,
+    _class: JClass
+) {
     android_logger::init_once(
         Config::default()
             .with_min_level(Level::Trace) // limit log level
@@ -27,7 +32,6 @@ fn native_activity_create() {
     );
 }
 
-
 #[no_mangle]
 pub unsafe extern fn Java_com_chaintope_tapyrus_spv_RustGreetings_greeting(env: JNIEnv, _: JClass, java_pattern: JString) -> jstring {
     // Our Java companion code might pass-in "world" as a string, hence the name.
@@ -35,6 +39,17 @@ pub unsafe extern fn Java_com_chaintope_tapyrus_spv_RustGreetings_greeting(env: 
     // Retake pointer so that we can use it below and allow memory to be freed when it goes out of scope.
     let world_ptr = CString::from_raw(world);
     let output = env.new_string(world_ptr.to_str().unwrap()).expect("Couldn't create java string!");
+
+    let params = Options {
+        remote: "192.168.0.44:18444".to_string(),
+        datadir: "/tmp/tapyrus-spv".to_string(),
+        chain_params: ChainParams {
+            network: Network::Regtest,
+        },
+    };
+
+    let spv = SPV::new(params);
+    spv.run();
 
     output.into_inner()
 }
